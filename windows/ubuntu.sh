2e41ee9e48887dea0b62fcc6febcc9102c37d694
Danny <madannyaguswahyudi@gmail.com>
2020-07-09 09:34:07 +0700

    add zsh

--- windows/ubuntu.sh
+++ windows/ubuntu.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+download_path=$HOME/Downloads
 
 # install fish shell
 sudo apt-add-repository ppa:fish-shell/release-3
@@ -10,10 +11,37 @@ fish=$(which fish)
 sh -s ${fish}
 
 # installing golang
-download_path=$HOME/Downloads
 mkdir -P ${download_path}
 wget -P ${download_path} https://golang.org/dl/go1.14.4.linux-amd64.tar.gz
 sudo tar -C /usr/local/ -xzf go1.14.4.linux-amd64.tar.gz
 
 # set export go for fish shell
 set -x PATH $PATH /usr/local/go/bin/
+
+# download .deb package for docker
+wget -P ${download_path} https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_19.03.12~3-0~ubuntu-focal_amd64.deb
+wget -P ${download_path} https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_19.03.12~3-0~ubuntu-focal_amd64.deb
+wget -P ${download_path} https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.2.13-2_amd64.deb
+
+# installing docker-cli and docker-ce
+sudo dpkg -i Downloads/docker-ce-cli_19.03.12\~3-0\~ubuntu-focal_amd64.deb 
+sudo dpkg -i Downloads/containerd.io_1.2.13-2_amd64.deb
+sudo dpkg -i Downloads/docker-ce_19.03.12\~3-0\~ubuntu-focal_amd64.deb
+
+# set docker to run without sudo
+sudo usermod -aG docker $(whoami)
+
+# installing zsh
+sudo apt-get update
+sudo apt upgrade
+sudo apt install zsh
+sudo apt-get install powerline fonts-powerline
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+# nano .zshrc
+# ZSH_THEME="agnoster"
+chsh -s /bin/zsh
+
+# syntax highlight 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
